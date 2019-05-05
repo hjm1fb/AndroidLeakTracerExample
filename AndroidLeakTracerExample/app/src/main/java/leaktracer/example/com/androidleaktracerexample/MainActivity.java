f8d78378709c5fbde788eaecfb51f3a6fd1801cd
kyle Hong <576678437@qq.com>
2019-05-06 00:57:51 +0800

    加入权限获取代码

--- AndroidLeakTracerExample/app/src/main/java/leaktracer/example/com/androidleaktracerexample/MainActivity.java
+++ AndroidLeakTracerExample/app/src/main/java/leaktracer/example/com/androidleaktracerexample/MainActivity.java
@@ -1,9 +1,16 @@
 package leaktracer.example.com.androidleaktracerexample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +33,18 @@ public class MainActivity extends AppCompatActivity {
                 testMemoryLeak();
             }
         });
+        new RxPermissions(this).request(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE)
+                .subscribe(new Consumer<Boolean>() {
+                    @Override
+                    public void accept(@NonNull Boolean aBoolean) throws Exception {
+                        if(!aBoolean) {
+                            Toast.makeText(MainActivity.this,"获取权限失败", Toast.LENGTH_LONG).show();
+                           finish();
+                        }else {
+
+                        }
+                    }
+                });
     }
 
     /**
--- README.md
+++ README.md
@@ -2,8 +2,3 @@
 Android Native  Memory Leak Detect Example of Leak Tracer 
 
 More info on Blog: https://www.jianshu.com/p/9058f0514416
-
-Permission-Request Code is missing, please grant app 
-READ_EXTERNAL_STORAGE & WRITE_EXTERNAL_STORAGE permission
-
-动态申请权限的代码还没写, 请给予APP文件读写权限.
